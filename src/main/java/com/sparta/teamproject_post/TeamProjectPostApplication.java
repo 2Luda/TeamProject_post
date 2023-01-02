6f7e29c6e6f63da8fbd053162080e5f23823bc05
jiho <shl05234@naver.com>
2023-01-02 09:46:31 +0900

    Initial commit

---
+++ src/main/java/com/sparta/teamproject_post/TeamProjectPostApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.teamproject_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/teamproject_post/TeamProjectPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.teamproject_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
