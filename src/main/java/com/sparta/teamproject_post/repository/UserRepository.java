ccf39c5d4a325bbd0a62e24a37d15ceca0119202
duyoung95 <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-01-04 12:23:29 +0900

    user
    userrepositiry 추가

---
+++ src/main/java/com/sparta/teamproject_post/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.teamproject_post.repository;
+
+import com.sparta.teamproject_post.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
