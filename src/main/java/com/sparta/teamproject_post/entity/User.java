8da0b2e39d9e4885eeed4eee01d96584108027e6
duyoung95 <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-01-04 12:15:56 +0900

    user entity

---
+++ src/main/java/com/sparta/teamproject_post/entity/User.java
@@ -0,0 +1,32 @@
+package com.sparta.teamproject_post.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Entity(name="USERS")
+@NoArgsConstructor
+@Getter
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private BigInteger user_id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String password, UserRoleEnum role){
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
+
+
